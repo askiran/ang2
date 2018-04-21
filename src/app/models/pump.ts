3fe17d5ebe18d6a98a8a6caef0022d08e39fa60f
SRIKANTH <srikanth.pavuluri@costrategix.com>
2018-04-22 00:31:13 +0530

    updated pump model

--- src/app/models/pump.ts
+++ src/app/models/pump.ts
@@ -1,17 +1,34 @@
 export interface Pump {
+    idpump: string;
     pump_no: string;
     capacity: string;
     voltage: string;
     current: string;
     water: string;
+    x1: string;
+    x2: string;
+    x3: string;
+    x4: string;
+    created_by: string;
+    created_date: string;
+    status: string;
     max_solar_energy: string;
     max_water_output: string;
-    latitude: string;
-    longitude: string;
     iduser: string;
     idsitemanager: string;
-    created_by: string;
-    status: string;
+    latitude: Number;
+    longitude: Number;
+    id: Number;
+    pump_id: string;
+    from_time: string;
+    to_time: string;
+    frequency: string;
+    read_data: string;
+    switchstatus: string;
+    mode: Boolean;
+    pumpUni: string;
+    createdby: string;
+    assignedSiteManager: string;
     phone: string;
     customer_name: string;
 }
